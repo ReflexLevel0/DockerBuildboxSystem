bec7790702f35a06665acbdd4f4264a585848ae0
ReflexLevel0 <dd54157@fer.hr>
2025-11-15 00:38:02 +0100

    Changed from static container status color to styled color

--- src/DockerBuildBoxSystem.App/Converters/StateToColorConverter.cs
+++ src/DockerBuildBoxSystem.App/Converters/StateToColorConverter.cs
@@ -1,30 +1,34 @@
 ﻿using DockerBuildBoxSystem.Contracts;
+using Microsoft.Extensions.Logging.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+
 
 namespace DockerBuildBoxSystem.App.Converters
 {
+    /// <summary>
+    /// Converts <see cref="ContainerState"/> into a string representation of a color
+    /// </summary>
     class StateToColorConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if(value is ContainerState)
+            if(value is not ContainerState)
             {
-                var state = (ContainerState)value;
-                switch(state)
-                {
-                    case ContainerState.Running:
-                        return "LimeGreen";
-                    default:
-                        return "Red";
-                }
+                return "";
             }
-            return null;
+
+            var state = (ContainerState)value;
+            object? color = state == ContainerState.Running ? 
+                Application.Current.Resources["SuccessColor"] : 
+                Application.Current.Resources["ErrorColor"];
+            return color == null ? "" : color.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
