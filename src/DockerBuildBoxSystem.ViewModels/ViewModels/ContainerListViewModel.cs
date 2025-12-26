500d2dee1c4efe53a5745f94aa61a2092aaa56fb
Alberto Kerim <40634753+AlbertoKerim@users.noreply.github.com>
2025-12-26 22:12:21 +0100

    Update ContainerListViewModel.cs

--- src/DockerBuildBoxSystem.ViewModels/ViewModels/ContainerListViewModel.cs
+++ src/DockerBuildBoxSystem.ViewModels/ViewModels/ContainerListViewModel.cs
@@ -283,11 +283,6 @@ namespace DockerBuildBoxSystem.ViewModels.ViewModels
             }
         }
 
-        /// <summary>
-        /// This will:
-        ///    set SelectedContainer = null if it was stopped externally
-        ///    set SelectedContainer = null if it was deleted externally
-        /// </summary>
         public async Task RefreshSelectedContainerAsync()
         {
             var id = ContainerId;
